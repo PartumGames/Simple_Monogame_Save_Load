dd70986aa60bd86c5cd2a6ae8678f3b2a166b484
PartumGames <37095216+PartumGames@users.noreply.github.com>
2021-02-20 15:17:03 -0600

    Add files via upload

---
+++ String_To_Hex/String_To_Hex/Data/Save_Data.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace String_To_Hex.Data
+{
+    
+    /// <summary>
+    /// Class we want to save
+    /// </summary>
+    public class Save_Data
+    {
+        public string usersName;
+        public float health;
+        public float mana;
+        public float hunger;
+        public float thirst;
+        public float stamina;
+        public int weaponId;
+
+        public Save_Data(string usersName, float health, float mana, float hunger, float thirst, float stamina, int weaponId)
+        {
+            this.usersName = usersName;
+            this.health = health;
+            this.mana = mana;
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.stamina = stamina;
+            this.weaponId = weaponId;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("user name: " + usersName);
+            sb.Append(" | health: " + health.ToString());
+            sb.Append(" | mana: " + mana.ToString());
+            sb.Append(" | hunger: " + hunger.ToString());
+            sb.Append(" | thirst: " + thirst.ToString());
+            sb.Append(" | stamina: " + stamina.ToString());
+            sb.Append(" | weaponID: " + weaponId.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ String_To_Hex/String_To_Hex/Data/Save_Load.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace String_To_Hex.Data
+{
+    //found the ToHexString() FromHexString() 
+    //here: https://stackoverflow.com/questions/16999604/convert-string-to-hex-string-in-c-sharp
+    //poster: franckspike
+
+    public class Save_Load<T>
+    {
+        private string savefile;
+
+        private bool useHex;
+
+        /// <summary>
+        /// Creates a new instance of Save_Load
+        /// </summary>
+        /// <param name="savefile">the full path to the save file, include filename and extension</param>
+        /// <param name="useHex">true = save as a hex string | false = save as a .json string</param>
+        public Save_Load(string savefile, bool useHex)
+        {
+            this.savefile = savefile;
+            this.useHex = useHex;
+        }
+
+
+        #region Save
+
+        /// <summary>
+        /// Saves to a file
+        /// </summary>
+        /// <param name="_gameData">The class that you want to save</param>
+        public void Save(T _gameData)
+        {
+            //to save
+            //--ToJson -> ToHex -> Push
+
+            if (useHex)
+            {
+                Push(ToHexString(ToJson(_gameData)));
+            }
+            else
+            {
+                Push(ToJson(_gameData));
+            }
+
+        }
+
+        private string ToJson(T _data)
+        {
+            return JsonConvert.SerializeObject(_data);
+        }
+
+        private string ToHexString(string str)
+        {
+            var sb = new StringBuilder();
+
+            var bytes = Encoding.Unicode.GetBytes(str);
+            foreach (var t in bytes)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+
+            return sb.ToString(); // returns: "48656C6C6F20776F726C64" for "Hello world"
+        }
+
+        private void Push(string _data)
+        {
+            using (StreamWriter sw = new StreamWriter(savefile))
+            {
+                sw.Write(_data);
+                sw.Close();
+            }
+        }
+
+        #endregion
+
+        #region Load
+
+        /// <summary>
+        /// Load the save file 
+        /// </summary>
+        /// <returns>returns save file as type T</returns>
+        public T Load()
+        {
+            //to load
+            //--Pull -> FromHex -> FromJson
+
+            if (useHex)
+            {
+                return FromJson(FromHexString(Pull()));
+            }
+            else
+            {
+                return FromJson(Pull());
+            }
+
+        }
+
+
+        private string Pull()
+        {
+            string _data = "";
+
+            using (StreamReader sr = new StreamReader(savefile))
+            {
+                _data = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            return _data;
+        }
+
+        private string FromHexString(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return Encoding.Unicode.GetString(bytes); // returns: "Hello world" for "48656C6C6F20776F726C64"
+        }
+
+        private T FromJson(string _data)
+        {
+            return JsonConvert.DeserializeObject<T>(_data);
+        }
+
+        #endregion
+
+
+    }
+}
